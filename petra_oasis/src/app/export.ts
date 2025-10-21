2c7fe2f8be7ec4a9a5e649778c61876c1e49905a
savyajajimoggala <savyajajimoggala@gmail.com>
2025-10-21 10:11:17 +0530

    created commitment file

---
+++ petra_oasis/src/app/export.ts
@@ -0,0 +1,29 @@
+export const commitment = [
+    {
+        "about1": "Quality Policy",
+        "para1": "MEP Enterprises is committed to meeting Customer Requirements through continual improvement of its Quality Management System.",
+        "para2": "MEP shall sustain Organizational Excellence through visionary leadership and innovative efforts.",
+        "about2": "Quality Assurance",
+        "para3": "The Company has a Quality Management System which complies with:",
+        "image": "/commitment.png",
+        "points": [
+            "All contractual obligations, Client’s Specifications and Industry standards.",
+            "All Statutory Regulations and Codes of Practice.",
+            "Providing and maintaining good & Clean environment and safe Workplace.",
+            "Maintaining high level of performance and professionalism by imparting training as well as providing adequate resources.",
+            "Continual improvement in the methods of working."
+        ]
+    },
+]
+
+export const commitmentcards = [
+    {
+        "para": "Our continuous learning and adaptation to technology empowers us to be competitive in the market. The knowledge we have from our qualified team enables us to complete the project successfully and on time."
+    },
+    {
+        "para": "Our people thrive on the challenge to excel in any environment, work on their special skills, are committed to common goals and good team players. We recruit experienced professionals and our aim is to constantly develop and empower our employees and create an environment where achievement is recognized and success is rewarded."
+    },
+    {
+        "para": "Our commitment to quality is one of the bases for our competitive advantage and customers build their trust on us. Our customers’ satisfaction is one of our prime importance."
+    }
+]
